4c6ba398223b72b2d931aa98eab146b157fbe398
wallas <wagratom22@gmail.com>
2023-04-11 11:38:07 -0300

    refactor functions

--- include/web_serve.hpp
+++ include/web_serve.hpp
@@ -6,7 +6,7 @@
 /*   By: wwallas- <wwallas-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 09:40:58 by wwallas-          #+#    #+#             */
-/*   Updated: 2023/04/11 10:14:39 by wwallas-         ###   ########.fr       */
+/*   Updated: 2023/04/11 11:18:41 by wwallas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 # include <sys/socket.h>
 # include <cstdio>
 # include <netinet/in.h>
-
+# include <iostream>
+# include <sys/epoll.h>
 
 bool	create_server(int& socket_dst);
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,20 +6,36 @@
 /*   By: wwallas- <wwallas-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 07:51:02 by wwallas-          #+#    #+#             */
-/*   Updated: 2023/04/11 10:39:58 by wwallas-         ###   ########.fr       */
+/*   Updated: 2023/04/11 11:30:33 by wwallas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <web_serve.hpp>
 
+bool	create_epoll(int& epoll_fd)
+{
+	epoll_fd = epoll_create(1);
+	if (epoll_fd != -1)
+		return (true);
+	std::cout << "Error: an epoll_create()" << std::endl;
+	return (false);
+}
+
+bool	create_epoll_configured(int& socket_fd, int& epoll_fd)
+{
+	if (create_epoll(epoll_fd))
+		return (false);
+	struct epoll_event event;
+}
+
 int main( void )
 {
-	int socket_desc;
+	int socket_fd;
 	int	epoll_fd;
 
-	if (create_server(socket_desc))
+	if (create_server(socket_fd))
 		return (1);
-	if (create_epoll(epoll_fd))
+	if (create_epoll_configured(socket_fd, epoll_fd))
 		return (1);
 	return(0)
 
