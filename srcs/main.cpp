9badaec3a9789ef46fe9be126d9e6bfe45b4bb1e
wallas <wagratom22@gmail.com>
2023-04-11 12:30:55 -0300

    create epoll

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,15 +6,24 @@
 /*   By: wwallas- <wwallas-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 07:51:02 by wwallas-          #+#    #+#             */
-/*   Updated: 2023/04/11 11:30:33 by wwallas-         ###   ########.fr       */
+/*   Updated: 2023/04/11 11:46:31 by wwallas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <web_serve.hpp>
 
+/*
+EPOLLIN - indica que há dados para serem lidos no descritor de arquivo.
+EPOLLOUT - indica que é possível escrever dados no descritor de arquivo sem bloqueio.
+EPOLLERR - indica que ocorreu um erro no descritor de arquivo.
+EPOLLHUP - indica que o descritor de arquivo foi fechado
+
+Edge-triggered mode (EPOLLET) - sinaliza um evento apenas quando ocorre uma mudança de estado, como a chegada de novos dados.
+Level-triggered mode (default) - sinaliza um evento enquanto o estado for verdadeiro, como a presença de dados para leitura.
+*/
 bool	create_epoll(int& epoll_fd)
 {
-	epoll_fd = epoll_create(1);
+	epoll_fd = epoll_create(5);
 	if (epoll_fd != -1)
 		return (true);
 	std::cout << "Error: an epoll_create()" << std::endl;
@@ -26,6 +35,8 @@ bool	create_epoll_configured(int& socket_fd, int& epoll_fd)
 	if (create_epoll(epoll_fd))
 		return (false);
 	struct epoll_event event;
+	event.data.fd = socket_fd;
+	event.events = EPOLLIN;
 }
 
 int main( void )
